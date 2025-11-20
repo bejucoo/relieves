fe9b43bee41ac05124f4532148d3c92ab9479c21
bejuco <pierrepuentes@gmail.com>
2025-11-20 17:21:44 -0500

    251120 driver.js steps changes

--- assets/js/onboarding.js
+++ assets/js/onboarding.js
@@ -8,31 +8,38 @@ const driverObj = driver({
 	showProgress: true,
 	steps: [
 		{
-			element: '#altimeter-container',
-			popover: {
-				title: 'Altímetro',
-				description: 'Lorem ipsum...'
-			}
-		},
-		{
 			element: '#user-altitude',
 			popover: {
 				title: 'Altura',
-				description: 'Lorem ipsum...'
+				description: 'Haciendo click aquí podrá obtener la altura del lugar donde se encuentra. El caso de estudio con la altura más cercana será destacado.'
+			}
+		},
+		{
+			element: '#altimeter-container',
+			popover: {
+				title: 'Altímetro',
+				description: 'En esta barra podrá seleccionar los casos según su altura sobre el nivel del mar haciendo click sobre cada círculo.'
 			}
 		},
 		{
 			element: '#selected-case',
 			popover: {
 				title: 'Seleccionado',
-				description: 'Lorem ipsum...'
+				description: 'Aquí se mostrará el nombre del caso seleccionado y la altura a la que se encuentra.'
 			}
 		},
 		{
 			element: '#feedback',
 			popover: {
 				title: 'Ir a caso',
-				description: 'Lorem ipsum...'
+				description: 'Haciendo click en este botón podrá ir a la página del caso seleccionado.'
+			}
+		},
+		{
+			element: '.case-image.active',
+			popover: {
+				title: 'Caso Seleccionado',
+				description: 'Las imágenes del caso seleccionado se mostrarán con color. Haciendo doble click también podrá ir a la página correspondiente.'
 			}
 		}
 	]
